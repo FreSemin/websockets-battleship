a7f592312459e3645f02996ae98718f19ac63fa8
fresemin <fresemin@gmail.com>
2024-02-24 15:21:30 +0300

    feat: use http port from .env

--- index.ts
+++ index.ts
@@ -1,6 +1,9 @@
 import { httpServer } from './src/http_server/index';
+import * as dotenv from 'dotenv';
 
-const HTTP_PORT = 8181;
+dotenv.config();
+
+const HTTP_PORT = Number(process.env.APP_HTTP_PORT) || 8181;
 
 httpServer.listen(HTTP_PORT, () => {
   console.log(`Start static http server on the ${HTTP_PORT} port!`);
