44cae6558bc6f7df77401ce3b960db08d5d1a226
fresemin <fresemin@gmail.com>
2024-02-25 19:17:23 +0300

    feat: app db entity

---
+++ src/db/db.ts
@@ -0,0 +1,20 @@
+import { UserRepository } from '../entities';
+
+export class AppDataBase {
+  private static instance: AppDataBase;
+
+  static getInstance(): AppDataBase {
+    if (!this.instance) {
+      this.instance = new AppDataBase();
+    }
+    return this.instance;
+  }
+
+  userRepository: UserRepository;
+
+  constructor() {
+    this.userRepository = new UserRepository();
+  }
+}
+
+export const AppDB: AppDataBase = AppDataBase.getInstance();
