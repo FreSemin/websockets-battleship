178ccfd10ba1f9c3e4ea291642bdf819e5c35e18
fresemin <fresemin@gmail.com>
2024-02-25 19:18:50 +0300

    feat: db service reg user

---
+++ src/db/db.service.ts
@@ -0,0 +1,62 @@
+import { WebSocket } from 'ws';
+import {
+  EMessageTypes,
+  MessageRes,
+  RegDataReq,
+  RegDataRes,
+} from '../wss/models';
+import { AppDB, AppDataBase } from '.';
+import { User } from '../entities';
+
+class AppDataBaseService {
+  private static instance: AppDataBaseService;
+
+  private appDB: AppDataBase;
+
+  // TODO: private or public?
+  public clients: Map<string, WebSocket> = new Map();
+
+  constructor() {
+    this.appDB = AppDB;
+  }
+
+  static getInstance(): AppDataBaseService {
+    if (!this.instance) {
+      this.instance = new AppDataBaseService();
+    }
+    return this.instance;
+  }
+
+  regUser(data: string, ws: WebSocket): void {
+    const parsedData: RegDataReq = JSON.parse(data);
+
+    const user: User = this.appDB.userRepository.create(parsedData);
+
+    this.regClient(ws, user);
+
+    this.sendRegRes(ws, user);
+
+    // TODO: update winners
+  }
+
+  regClient(ws: WebSocket, user: User): void {
+    this.clients.set(user.index, ws);
+  }
+
+  sendRegRes(ws: WebSocket, user: User): void {
+    const regResponse: MessageRes<RegDataRes> = new MessageRes<RegDataRes>(
+      EMessageTypes.reg,
+      {
+        index: user.index,
+        name: user.name,
+        error: false,
+        errorText: '',
+      },
+    );
+
+    ws.send(regResponse.toJSON());
+  }
+}
+
+export const AppDBService: AppDataBaseService =
+  AppDataBaseService.getInstance();
