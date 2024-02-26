d630bce34a3482083a57231dbe777453236b6820
fresemin <fresemin@gmail.com>
2024-02-26 15:24:23 +0300

    feat: room entity add user to room

--- src/db/entities/room.entity.ts
+++ src/db/entities/room.entity.ts
@@ -1,5 +1,5 @@
 import { IBaseRepository } from '.';
-import { Room } from '../../models';
+import { Room, User } from '../../models';
 
 export class RoomRepository implements IBaseRepository<Room> {
   private rooms: Room[] = [];
@@ -7,6 +7,18 @@ export class RoomRepository implements IBaseRepository<Room> {
   findAll(): Room[] {
     // TODO: add number to constants
     return this.rooms.filter((room) => room.roomUsers.length < 2);
+  }
+
+  addUserToRoom(roomId: string, user: User): void {
+    const roomIndex: number = this.rooms.findIndex(
+      (room) => room.roomId === roomId,
+    );
+
+    if (roomIndex !== -1 && this.rooms[roomIndex].roomUsers.length < 2) {
+      this.rooms[roomIndex].roomUsers.push(user);
+    } else {
+      // TODO: throw error
+    }
   }
 
   create(): void {
